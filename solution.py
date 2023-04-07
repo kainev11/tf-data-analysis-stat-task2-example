f696ed470452d6d8576cbe7d5f9ef38f1d36ed30
Evgeny Nikolaev <50527841+kainev11@users.noreply.github.com>
2023-04-08 03:37:14 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -13,9 +13,10 @@ def solution(p: float, x: np.array) -> tuple:
     alpha = 1 - p
     n = len(x)
     x_mean = np.mean(x)
-    x_var = np.var(x, ddof=1) # несмещенная оценка дисперсии
+    x_var = np.var(x, ddof=1)
     chi2_l = chi2.ppf(alpha / 2, df=n - 1)
     chi2_r = chi2.ppf(1 - alpha / 2, df=n - 1)
     s_l = np.sqrt((n - 1) * x_var / chi2_r)
     s_r = np.sqrt((n - 1) * x_var / chi2_l)
-    return x_mean - s_l, x_mean + s_r
+    interval_len = s_l + s_r
+    return x_mean - s_l, x_mean + s_r, interval_len
