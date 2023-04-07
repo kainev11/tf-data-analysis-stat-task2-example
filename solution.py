540edbf7f28e8955c2baba6a30b5030955746418
Evgeny Nikolaev <50527841+kainev11@users.noreply.github.com>
2023-04-08 03:41:10 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -12,11 +12,9 @@ def solution(p: float, x: np.array) -> tuple:
     # Не меняйте название функции и её аргументы
     alpha = 1 - p
     n = len(x)
-    x_mean = np.mean(x)
-    x_var = np.var(x, ddof=1)
-    chi2_l = chi2.ppf(alpha / 2, df=n - 1)
-    chi2_r = chi2.ppf(1 - alpha / 2, df=n - 1)
-    s_l = np.sqrt((n - 1) * x_var / chi2_r)
-    s_r = np.sqrt((n - 1) * x_var / chi2_l)
-    interval_len = s_l + s_r
-    return x_mean - s_l, x_mean + s_r, interval_len
+    mean = np.mean(x)
+    var = np.var(x, ddof=1)
+    std_err = np.sqrt(var / n)
+    z = norm.ppf(1 - alpha / 2)
+    ci = norm.interval(alpha, loc=mean, scale=std_err)
+    return ci
