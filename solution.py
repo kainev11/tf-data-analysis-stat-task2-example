9f00892ddab7e56f0779bed2b696b080c9dde1c4
Evgeny Nikolaev <50527841+kainev11@users.noreply.github.com>
2023-04-08 03:20:24 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,17 +1,21 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import norm
+from scipy.stats import chi2, norm
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 294776608 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    n = len(x)
+    x_mean = np.mean(x)
+    x_var = np.var(x, ddof=1) # несмещенная оценка дисперсии
+    chi2_l = chi2.ppf(alpha / 2, df=n - 1)
+    chi2_r = chi2.ppf(1 - alpha / 2, df=n - 1)
+    s_l = np.sqrt((n - 1) * x_var / chi2_r)
+    s_r = np.sqrt((n - 1) * x_var / chi2_l)
+    return x_mean - s_l, x_mean + s_r
